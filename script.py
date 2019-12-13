9fcacf98e3cc52de925caa800d916baa1687410b
Gonçalo Santos <gregueiras@gmail.com>
2019-12-13 16:55:30 +0000

    Add line counter to script

--- script.py
+++ script.py
@@ -7,6 +7,20 @@ MAX_WORKERS = 4
 NO_RUNS = 200
 INITIAL_NO_RUNS = 200
 
+#Count lines in a folder
+def bufcount(filename):
+    f = open(filename)                  
+    lines = 0
+    buf_size = 1024 * 1024
+    read_f = f.read # loop optimization
+
+    buf = read_f(buf_size)
+    while buf:
+        lines += buf.count('\n')
+        buf = read_f(buf_size)
+
+    return lines
+
 # Print iterations progress
 def printProgressBar (iteration, total, prefix = '', suffix = '', decimals = 1, length = 100, fill = '█', printEnd = "\r"):
     """
@@ -24,7 +38,7 @@ def printProgressBar (iteration, total, prefix = '', suffix = '', decimals = 1, 
     percent = ("{0:." + str(decimals) + "f}").format(100 * (iteration / float(total)))
     filledLength = int(length * iteration // total)
     bar = fill * filledLength + '-' * (length - filledLength)
-    print('\r%s |%s| %s%% %s' % (prefix, bar, percent, suffix), end = printEnd)
+    print('\r%s |%s| %s%% %s' % (prefix, bar, percent, f'{suffix} {bufcount("./logs/GameData.txt")}'), end = printEnd)
     # Print New Line on Complete
     if iteration == total:
         print()
@@ -52,7 +66,7 @@ def freeMutex():
     global MAX_WORKERS
     MAX_WORKERS += 1
     NO_RUNS -= 1
-    printProgressBar(INITIAL_NO_RUNS - NO_RUNS, INITIAL_NO_RUNS + MAX_WORKERS, prefix = 'Progress:', suffix = f'Complete\tAvailable Runners: {MAX_WORKERS}', length = 100)
+    printProgressBar(INITIAL_NO_RUNS - NO_RUNS, INITIAL_NO_RUNS + MAX_WORKERS, prefix = 'Progress:', suffix = 'Complete', length = 100)
 
 
 def main():
@@ -61,12 +75,12 @@ def main():
 
     command = r'"C:\Program Files\JetBrains\IntelliJ IDEA 2019.2.3\jbr\bin\java.exe" "-javaagent:C:\Program Files\JetBrains\IntelliJ IDEA 2019.2.3\lib\idea_rt.jar=55408:C:\Program Files\JetBrains\IntelliJ IDEA 2019.2.3\bin" -Dfile.encoding=UTF-8 -classpath C:\Users\gonca\IdeaProjects\AIAD\target\classes;C:\Users\gonca\.m2\repository\com\tilab\jade\jade\4.5.0\jade-4.5.0.jar;C:\Users\gonca\.m2\repository\commons-codec\commons-codec\1.13\commons-codec-1.13.jar;C:\Users\gonca\.m2\repository\org\json\json\20190722\json-20190722.jar;C:\Users\gonca\.m2\repository\com\google\code\gson\gson\2.8.6\gson-2.8.6.jar;C:\Users\gonca\.m2\repository\commons-io\commons-io\2.6\commons-io-2.6.jar;C:\Users\gonca\.m2\repository\commons-cli\commons-cli\1.4\commons-cli-1.4.jar Main"'
 
-    printProgressBar(INITIAL_NO_RUNS - NO_RUNS, INITIAL_NO_RUNS + MAX_WORKERS, prefix = 'Progress:', suffix = f'Complete\tAvailable Runners: {MAX_WORKERS}', length = 100)
+    printProgressBar(INITIAL_NO_RUNS - NO_RUNS, INITIAL_NO_RUNS + MAX_WORKERS, prefix = 'Progress:', suffix = 'Complete', length = 100)
     while NO_RUNS > 0:
         if (MAX_WORKERS > 0):
             popenAndCall(freeMutex, command)
             MAX_WORKERS -= 1
-            printProgressBar(INITIAL_NO_RUNS - NO_RUNS, INITIAL_NO_RUNS + MAX_WORKERS, prefix = 'Progress:', suffix = f'Complete\tAvailable Runners: {MAX_WORKERS}', length = 100)
+            printProgressBar(INITIAL_NO_RUNS - NO_RUNS, INITIAL_NO_RUNS + MAX_WORKERS, prefix = 'Progress:', suffix = 'Complete', length = 100)
 
     print("program continued")
     sys.exit(0)
